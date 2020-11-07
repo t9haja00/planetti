aa5a204c7a75fba95b048ebcafc85ed0d5d28e64
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-07 18:07:36 +0200

    Add Database script for creating users table

--- db-script/planetti-db-script.sql
+++ db-script/planetti-db-script.sql
@@ -1,2 +1,15 @@
 -- Planetti Database Script
 -----------------------------
+
+DROP TABLE users;
+
+CREATE DATABASE planetti;
+
+CREATE TABLE IF NOT EXISTS users (
+id  SERIAL PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+password VARCHAR(100) NOT NULL,
+created_on TIMESTAMP NOT NULL,
+last_login TIMESTAMP 
+);

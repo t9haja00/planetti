5cd08083561fb0dfa7c23778db721e7eb6503ef6
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-07 17:14:40 +0200

    Add httpService for modularity and to handle global unexpected errors

---
+++ planetti-ui/src/services/httpService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+// handle unexpected error globally
+  axios.interceptors.response.use(null, err => {
+    const expectedError = 
+      err.response &&
+      err.response.status >= 400 &&
+      err.response.status < 500;
+
+      if (!expectedError) {
+        console.log('Logging unexpected error ', err);
+        toast.error('An unexpected error occured.');
+      }
+
+      return Promise.reject(err);
+  });
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete
+}

53447766f41a53638b7edaad788a95c56a7edd71
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-07 17:15:26 +0200

    Add userService to handle all users related api calls

---
+++ planetti-ui/src/services/userService.js
@@ -0,0 +1,9 @@
+import http from './httpService';
+
+const usersApi = process.env.REACT_APP_USERS;
+
+export function signup(user) {
+  const { name, email, password } = user;
+
+  return http.post(usersApi, { name, email, password });
+}

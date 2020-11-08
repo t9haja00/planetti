4ebb70c87e3ce2caa2cfa32b26ec1d19bfbb11bb
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-08 17:35:28 +0200

    Import auth router to index.js

--- planetti-api/index.js
+++ planetti-api/index.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3200;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/users', users);
+app.use('/auth', auth);
 
 console.log('Database connection is opened successfully!');
 app.listen(PORT, () => console.log(`Server is up and listening on ${PORT}...`));

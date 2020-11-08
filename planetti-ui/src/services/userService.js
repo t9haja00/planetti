4fedb19786536c2c33ad11535a4122ae100dfa80
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-08 17:36:19 +0200

    Change emails to lower case before committing to database

--- planetti-ui/src/services/userService.js
+++ planetti-ui/src/services/userService.js
@@ -5,5 +5,9 @@ const usersApi = process.env.REACT_APP_USERS;
 export function signup(user) {
   const { name, email, password } = user;
 
-  return http.post(usersApi, { name, email, password });
+  return http.post(usersApi, {
+    name,
+    email: email.toLowerCase(),
+    password
+  });
 }

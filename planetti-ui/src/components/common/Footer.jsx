82f20ce7ef530e63b778c087db32aa89373a301e
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-08 17:01:24 +0200

    Create conditional rendering for footer

--- planetti-ui/src/components/common/Footer.jsx
+++ planetti-ui/src/components/common/Footer.jsx
@@ -3,27 +3,40 @@ import styles from '../../assets/css/footer.module.css';
 
 const iconsUrl = process.env.REACT_APP_API;
 
-const Footer = () => {
-  return (
-    <div className='footer mt-auto'>
-      <div className={styles.main}>
-        <div className="d-flex container justify-content-between flex-row-reverse align-items-center py-3">
-          <ul className="list-unstyled d-flex justify-content-center mb-0">
-            <li className="mr-3">
-              <img className={styles.icon} src={`${iconsUrl}/icons/facebook.svg`} />
-            </li>
-            <li className="mr-3">
-              <img className={styles.icon} src={`${iconsUrl}/icons/linkedin.svg`} />
-            </li>
-            <li className="mr-3">
-              <img className={styles.icon} src={`${iconsUrl}/icons/github.svg`} />
-            </li>
-          </ul>
-          <div>
-            <small className="text-muted">© 2020 Planetti Inc.</small>
+const Footer = ({ match }) => {
+  const footer = () => {
+    if (match.url !== '/Login') {
+      return (
+        <div className={styles.main}>
+          <div className="d-flex container justify-content-between flex-row-reverse align-items-center py-3">
+            <ul className="list-unstyled d-flex justify-content-center mb-0">
+              <li className="mr-3">
+                <img className={styles.icon} src={`${iconsUrl}/icons/facebook.svg`} alt='' />
+              </li>
+              <li className="mr-3">
+                <img className={styles.icon} src={`${iconsUrl}/icons/linkedin.svg`} alt='' />
+              </li>
+              <li className="mr-3">
+                <img className={styles.icon} src={`${iconsUrl}/icons/github.svg`} alt='' />
+              </li>
+            </ul>
+            <div>
+              <small className="text-muted">© 2020 Planetti Inc.</small>
+            </div>
           </div>
         </div>
-      </div>
+      )
+    }
+
+      return (
+            <div className="container text-center mt-4">
+              <small className="text-muted">© 2020 Planetti Inc.</small>
+            </div>
+      )
+  }
+  return (
+    <div className='footer mt-auto'>
+      {footer()}
     </div>
   );
 };

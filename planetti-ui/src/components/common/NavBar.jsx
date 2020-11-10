1444c120cdda7707ed1f2c8fbeac5a8318bdba15
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-10 17:30:58 +0200

    Set conditional className for guest user

--- planetti-ui/src/components/common/NavBar.jsx
+++ planetti-ui/src/components/common/NavBar.jsx
@@ -7,47 +7,52 @@ const NavBar = ({ siteName = '', user, onLogout }) => {
 
   const [expanded, setExpanded] = useState(false);
 
+  const className = !user ? "container position-absolute py-2" :
+                        "";
+
   return (
-    <Navbar variant="dark" expanded={expanded} className={styles.navbar}>
-      <Navbar.Brand>
-        <Link className={styles['navbar-brand']} to='/' onClick={() => setExpanded(false)}>
-          {siteName}
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"
-        onClick={() => setExpanded(expanded ? false : "expanded")} />
-      <Navbar.Collapse className="justify-content-end">
-        <Nav>
-          {!user &&
-            <>
-              <Link
-                className={`${styles.login } ${styles['navbar-link']} btn mx-1`}
-                to='/Login'
-                name='Login'
-                onClick={() => setExpanded(false)}>Sign in</Link>
-              <Link
-                className={`${styles.join} ${styles['navbar-link']} btn rounded-pill mx-1`}
-                to='/Join'
-                name='Join'
-                onClick={() => setExpanded(false)}>Sing up</Link>
-            </>
-          }
-          {user &&
-            <>
-              <button
-                className='border rounded-sm mx-1'
-                name='user'>
-                {user}
-              </button>
-              <button
-                className='border rounded-sm mx-1'
-                name='Logout'
-                onClick={onLogout}>Logout</button>
-            </>
-          }
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className={className} style={{right: 0, left: 0}}>
+      <Navbar variant="dark" expanded={expanded} className={styles.navbar}>
+        <Navbar.Brand>
+          <Link className={styles['navbar-brand']} to='/' onClick={() => setExpanded(false)}>
+            {siteName}
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(expanded ? false : "expanded")} />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            {!user &&
+              <>
+                <Link
+                  className={`${styles.login} ${styles['navbar-link']} btn mx-1`}
+                  to='/Login'
+                  name='Login'
+                  onClick={() => setExpanded(false)}>Sign in</Link>
+                <Link
+                  className={`${styles.join} ${styles['navbar-link']} btn rounded-pill mx-1`}
+                  to='/Join'
+                  name='Join'
+                  onClick={() => setExpanded(false)}>Sing up</Link>
+              </>
+            }
+            {user &&
+              <>
+                <button
+                  className='border rounded-sm mx-1'
+                  name='user'>
+                  {user}
+                </button>
+                <button
+                  className='border rounded-sm mx-1'
+                  name='Logout'
+                  onClick={onLogout}>Logout</button>
+              </>
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
 

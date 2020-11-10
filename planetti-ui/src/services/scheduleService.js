15f4d79502a3845cdcbbb425748423369edb3db9
Jami Hannus <t9haja00@students.oamk.fi>
2020-11-10 19:23:30 +0200

    first phase of schedule service

---
+++ planetti-ui/src/services/scheduleService.js
@@ -0,0 +1,22 @@
+import http from "./httpService";
+//where does the  schedules api happen
+const schedulesAPI = process.env.REACT_APP_SCHEDULES;
+
+//get all schedules with this user id
+export function getSchedules(user_id) {
+  return http.get(schedulesAPI, {}, { 
+      user_id
+  });
+}
+//create new schedule for this user_id
+export function newSchedule(user_id) {
+    return http.post(schedulesAPI, {}, { 
+        user_id
+    });
+  }
+  //delete 1 schedule with the selected id
+  export function deleteSchedule(schedule_id) {
+    return http.delete(schedulesAPI, {}, { 
+        schedule_id
+    });
+  }

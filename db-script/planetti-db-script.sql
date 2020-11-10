39e87c9b30faa04d226a5805a0cdaf204475b399
AlexanderV <t9veal00@students.oamks.fi>
2020-11-10 14:50:00 +0200

    updated database create code

--- db-script/planetti-db-script.sql
+++ db-script/planetti-db-script.sql
@@ -20,25 +20,24 @@ CREATE DATABASE planetti;
 
 
 CREATE TABLE IF NOT EXISTS  users (
-	user_id int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-	"name" varchar(30) NOT NULL,
+	user_id SERIAL PRIMARY KEY,
+	name varchar(30) NOT NULL,
 	email varchar(50) NOT NULL,
-	"password" varchar(100) NOT NULL,
+	password varchar(100) NOT NULL,
 	created_on timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT users_pkey PRIMARY KEY (user_id)
 );
 
 -- public.schedules definition
 
 
 CREATE TABLE IF NOT EXISTS  schedules (
-	schedule_id serial NOT NULL DEFAULT nextval('schedules_schedule_id_seq'::regclass),
+	schedule_id SERIAL PRIMARY KEY,
 	uuid varchar(45) NOT NULL,
 	title varchar(255) NOT NULL,
 	description varchar(255) NULL,
 	create_time timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
-	"user" int4 NOT NULL,
-	CONSTRAINT schedules_pk PRIMARY KEY (schedule_id)
+	user int4 NOT NULL,
+
 );
 
 
@@ -50,11 +49,10 @@ ALTER TABLE public.schedules ADD CONSTRAINT schedules_fk FOREIGN KEY ("user") RE
 
 
 CREATE TABLE IF NOT EXISTS  events (
-	event_id serial NOT NULL DEFAULT nextval('events_event_id_seq'::regclass),
-	"event" json NOT NULL,
+	event_id serial PRIMARY KEY,
+	event json NOT NULL,
 	create_time timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
 	schedule int4 NOT NULL,
-	CONSTRAINT events_pk PRIMARY KEY (event_id)
 );
 
 

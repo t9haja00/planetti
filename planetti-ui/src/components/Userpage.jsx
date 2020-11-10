9441b337bcbc6ee7b49363e36b8c65ca5016a714
Jami Hannus <t9haja00@students.oamk.fi>
2020-11-10 20:29:14 +0200

    base of user page by fetching the schudules

---
+++ planetti-ui/src/components/Userpage.jsx
@@ -0,0 +1,21 @@
+/* Packages
+------------*/
+import React, { useEffect, useState } from 'react';
+/* Components
+--------------*/
+//wont need thease most likely? will have conditional rendering?
+
+import { getSchedules } from '../services/scheduleService';
+
+/* Styles
+----------*/
+
+
+
+
+useEffect(() => {
+    const { data: schedules } = await signin(user_id);
+},
+[]);
+
+export default  Userpage;

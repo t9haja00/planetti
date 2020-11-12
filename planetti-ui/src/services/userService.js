d6a0f5b0be15088977acfbb604a84c55ff18b20c
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-12 20:02:33 +0200

    Create service to get user by id and  change user name and email

--- planetti-ui/src/services/userService.js
+++ planetti-ui/src/services/userService.js
@@ -2,6 +2,10 @@ import http from './httpService';
 
 const usersApi = process.env.REACT_APP_USERS;
 
+export function getUserInfoById(userId) {
+  return http.get(`${usersApi}/${userId}`);
+}
+
 export function signup(user) {
   const { name, email, password } = user;
 
@@ -9,5 +13,13 @@ export function signup(user) {
     name,
     email: email.toLowerCase(),
     password
+  });
+}
+
+export function changeNameAndEmail(userInfo, userId) {
+  const { name, email } =  userInfo;
+  return http.put(`${usersApi}/${userId}`, {
+    name,
+    email
   });
 }

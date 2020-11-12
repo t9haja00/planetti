40465a6b203b22db6f59c5b8f1449b910ebc5d1c
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-12 23:30:13 +0200

    Write delete account handler

--- planetti-ui/src/components/DeleteAccount.jsx
+++ planetti-ui/src/components/DeleteAccount.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import {deleteUserAccount} from '../services/userService';
 
 const DeleteAccount = () => {
-  return null;
+
+  const handleDelete = async () => {
+    const userInfo = localStorage.getItem('userInfo');
+    const { user_id } = JSON.parse(userInfo);
+
+    await deleteUserAccount(user_id);
+    localStorage.removeItem('userInfo');
+    window.location = '/';
+    successNote();
+  };
+
+  const successNote = () => {
+    toast.dark(('Saved')
+    );
+  };
+
+  return (
+    <div>
+      <h3>Once you delete your account, there is no going back. Please be certain.
+      </h3>
+      <button onClick={handleDelete}>
+        DELETE
+      </button>
+    </div>
+  );
 }
 
 export default DeleteAccount;

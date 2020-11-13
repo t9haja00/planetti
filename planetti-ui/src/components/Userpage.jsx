aebbdceff28bf2253c2205ee19e09fad2b7d4028
Jami Hannus <t9haja00@students.oamk.fi>
2020-11-13 11:53:43 +0200

    bit more working but schedules.map not function

--- planetti-ui/src/components/Userpage.jsx
+++ planetti-ui/src/components/Userpage.jsx
@@ -3,19 +3,45 @@
 import React, { useEffect, useState } from 'react';
 /* Components
 --------------*/
-//wont need thease most likely? will have conditional rendering?
-
 import { getSchedules } from '../services/scheduleService';
+import SingleSchedule from './SingleSchedule';
+
 
 /* Styles
 ----------*/
+  
 
-user_id = localStorage.getItem('userId');
+const Userpage = () => {
 
+    const [schedules, setSchedules] = useState(
+        ({schedule_id :"1",
+        uuid:"1",
+        title:"1",
+        description:"1",
+        create_time:"1",
+        user_id:"1",}))
 
-useEffect(() => {
-    const { data: schedules } = await signin(user_id);
-},
-[]);
+    // useEffect( async () => {
+    //     const user_id = localStorage.getItem('userId');
+    //     const {data} =this.schedules;
+    //     const {data:test}  = await getSchedules(user_id);
+    //     setSchedules(test)
+    //       }, [])
+ 
+    console.log(schedules)
+    return(
+        <div>
+            {schedules.map(single => (
+            <SingleSchedule key ={single.schedule_id}{...single}/>
+            ))
+            }
+            <div>
+                <button href ="/Add_schedule">
+                Add schedule
+                </button>
+            </div>
+           </div>
+            )       
+}
 
 export default  Userpage;

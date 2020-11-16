3e6ac4443a98344e5c41a61bef7c85cd8fe0cc67
Alex-Vel <t9veal00@students.oamk.fi>
2020-11-16 16:36:02 +0200

    changed db script json to jsonb + updated put req

--- planetti-api/routes/events.js
+++ planetti-api/routes/events.js
@@ -1,64 +1,65 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db/index');
+const db = require("../db/index");
 
 // get events for schedule
-router.get('/:id', (req, res) => {
-const schedule_id = req.params.id;
 
-  db.query('SELECT * FROM events WHERE schedule_id=($1)',[schedule_id])
-    .then(events => {
+router.get("/:id", (req, res) => {
+  const schedule_id = req.params.id;
+
+  db.query("SELECT * FROM events WHERE schedule_id=($1)", [schedule_id]).then(
+    (events) => {
       res.send(events.rows);
-    })
+    }
+  );
 });
 
 // create an event for a schedule
-router.post('/', (req, res) => {
-  
+
+router.post("/", (req, res) => {
   const eventObject = req.body.event;
   const schedule_id = req.body.schedule_id;
 
-  console.log(eventObject);
-  const newEvent = [
-    eventObject,
-    schedule_id
-  ];
+  const newEvent = [eventObject, schedule_id];
+  console.log(newEvent);
 
-      db.query('INSERT INTO events (event, schedule_id) VALUES ($1, $2)', newEvent)
-        .then(() => res.sendStatus(201))
-        .catch(err => {
-          console.log('Create new event error ', err);
-          res.sendStatus(500);
-        });
-
-
+  db.query("INSERT INTO events (event, schedule_id) VALUES ($1, $2)", newEvent)
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log("Create new event error ", err);
+      res.sendStatus(500);
+    });
 });
 
 //delete an event
 
-router.delete('/:id',  (req, res) => {
-    console.log(req.params);
-    const event_id = req.params.id;
-    
-    db.query('DELETE FROM events WHERE event_id=($1)', [event_id])
-    
-        .then(() => res.sendStatus(200))
-        .catch(err => {
-          console.log('Delete event error ', err);
-          res.sendStatus(500);
-        });
+router.delete("/:id", (req, res) => {
+  console.log(req.params);
+  const event_id = req.params.id;
 
+  db.query("DELETE FROM events WHERE event_id=($1)", [event_id])
+
+    .then(() => res.sendStatus(200))
+    .catch((err) => {
+      console.log("Delete event error ", err);
+      res.sendStatus(500);
+    });
 });
 
-//edit an event object
-// router.put('/:id', (req, res) => {
-//   const event_id = req.params.id;
-
-//   db.query('SELECT * FROM events WHERE event_id=($1)',[event_id])
-
-//     .then(events => {
-//       res.send(events.rows);
-//     })
-// });
+// edit an event object
+router.put('/:id', (req, res) => {
+  const event_id = req.params.id;
+  const editEvent = req.body;
+  console.log(editEvent);
+  db.query('SELECT event FROM events WHERE event_id=($1)',[event_id]) 
+    .then(() => {
+    db.query('UPDATE events SET event=($1)' ,[editEvent]) 
+    res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("edit event error ", err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;

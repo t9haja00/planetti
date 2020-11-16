2fb46d2ea7139427bf8b5dcce6b10be45d7d9f7f
Jami Hannus <t9haja00@students.oamk.fi>
2020-11-16 17:47:24 +0200

    clean up

--- planetti-ui/src/services/scheduleService.js
+++ planetti-ui/src/services/scheduleService.js
@@ -5,7 +5,7 @@ const schedulesAPI = process.env.REACT_APP_SCHEDULES;
 //get all schedules with this user id
 export function getSchedules(user_id)
 {
- return http.get(`${schedulesAPI}/${user_id}`, {user_id});
+ return http.get(`${schedulesAPI}/${user_id}`);
 }
 //create new schedule for this user_id
 export function newSchedule(schedule) {
@@ -19,7 +19,7 @@ export function newSchedule(schedule) {
   //delete 1 schedule with the selected id
   export function deleteSchedule(schedule_id)
   {
-    return http.delete(`${schedulesAPI}/${schedule_id}`,{schedule_id},);
+    return http.delete(`${schedulesAPI}/${schedule_id}`);
   }
 
 

fd6a7ab96aa94fe248389113b2d6e734fa6d1036
Jami Hannus <t9haja00@students.oamk.fi>
2020-11-16 14:09:29 +0200

    lisäsin moduuli pop-up ikkunan vahvistamaan poisto komennon

--- planetti-ui/src/components/Userpage.jsx
+++ planetti-ui/src/components/Userpage.jsx
@@ -1,6 +1,7 @@
 /* Packages
 ------------*/
 import React, { useEffect, useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 /* Components
 --------------*/
 import { getSchedules } from "../services/scheduleService";
@@ -10,43 +11,72 @@ import { deleteSchedule } from "../services/scheduleService";
 
 /* Styles
 ----------*/
+import styles from "../assets/css/delete-account.module.css";
 
 const Userpage = () => {
   //Using the dom history to push the path
   const history = useHistory();
   const [schedules, setSchedules] = useState([]);
-
+  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+  const [scheduleIDDelete, setscheduleIdDelete] = useState();
   const routeChange = () => {
     let path = `/Add_schedule`;
     history.push(path);
   };
 
   useEffect(async () => {
-    console.log(" inside the Use effect ");
     const id = localStorage.getItem("userId");
     const { data } = await getSchedules(id);
     //so gets all the schedules for given user id
     setSchedules(data);
   }, []);
 
-  const handleDeleteSchedule = async (schedule_id) => {
+  const handleClose = () => {
+    setShowDeleteConfirm(false);
+  };
+
+  const handleShow = (schedule_id) => {
+    setShowDeleteConfirm(true);
+    setscheduleIdDelete(schedule_id);
+  };
+  const handleDeleteSchedule = async () => {
+    const schedule_id = scheduleIDDelete;
     await deleteSchedule(schedule_id);
-    const id = localStorage.getItem("userId");
-    const { data } = await getSchedules(id);
+    const userInfo = localStorage.getItem("userInfo");
+    const { user_id } = JSON.parse(userInfo);
+    const { data } = await getSchedules(user_id);
     setSchedules(data);
+    handleClose();
   };
 
   return (
     <div>
       {schedules.map((single) => (
         <SingleSchedule
-          deleteSchedule={handleDeleteSchedule}
+          deleteSchedule={handleShow}
           key={single.schedule_id}
           {...single}
         />
       ))}
       <div>
         <button onClick={routeChange}>Add new schedule</button>
+      </div>
+      <div>
+        <Modal centered show={showDeleteConfirm} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title className={styles["modal-title"]}>
+              Are you sure you want to delete this schedule?
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className={styles["modal-body"]}>
+            <Button className="btn-danger" onClick={handleDeleteSchedule}>
+              Delete schedule
+            </Button>
+            <Button className={styles.cancel} onClick={handleClose}>
+              Cancel
+            </Button>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );

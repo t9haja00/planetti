e8e84deca31f210ca5abc4013e6cb025c6bb6d5a
Alex-Vel <t9veal00@students.oamk.fi>
2020-11-20 14:06:02 +0200

    added optional params to route path

--- planetti-api/index.js
+++ planetti-api/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/users', users);
 app.use('/auth', auth);
 app.use('/schedules',schedules);
-app.use('/events', events);
+app.use('/:uuid?/events/', events);
 
 console.log('Database connection is opened successfully!');
 app.listen(PORT, () => console.log(`Server is up and listening on ${PORT}...`));

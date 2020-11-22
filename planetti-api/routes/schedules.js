37e4ee42454b836d40cbcf16c3ea521a3fc6f323
Alex-Vel <t9veal00@students.oamk.fi>
2020-11-22 13:51:55 +0200

    Editted the put request so it edits one schedule

--- planetti-api/routes/schedules.js
+++ planetti-api/routes/schedules.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db/index');
-const { v4: uuidv4 } = require('uuid');
+const db = require("../db/index");
+const { v4: uuidv4 } = require("uuid");
 
 // get schedules for user
-router.get('/:id', (req, res) => {
-const user_id = req.params.id;
+router.get("/:id", (req, res) => {
+  const user_id = req.params.id;
 
-  db.query('SELECT * FROM schedules WHERE user_id=($1)',[user_id])
-    .then(schedules => {
+  db.query("SELECT * FROM schedules WHERE user_id=($1)", [user_id])
+    .then((schedules) => {
       res.send(schedules.rows);
     })
     .catch((err) => {
@@ -18,56 +18,51 @@ const user_id = req.params.id;
 });
 
 // create a schedule for a user
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   console.log(req.body);
-  const {title, description, user_id} = req.body;
+  const { title, description, user_id } = req.body;
   const uuid = uuidv4();
-  const newSchedule = [
-    title,
-    description,
-    uuid,
-    user_id
-  ];
+  const newSchedule = [title, description, uuid, user_id];
 
-      db.query('INSERT INTO schedules (title, description, uuid, user_id) VALUES ($1, $2, $3, $4) ', newSchedule)
-        .then(() => res.sendStatus(201))
-        .catch(err => {
-          console.log('Create new schedule error ', err);
-          res.sendStatus(500);
-        });
-
-
+  db.query(
+    "INSERT INTO schedules (title, description, uuid, user_id) VALUES ($1, $2, $3, $4) ",
+    newSchedule
+  )
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log("Create new schedule error ", err);
+      res.sendStatus(500);
+    });
 });
 
-//delete a schedule 
+//delete a schedule
 
-router.delete('/:id',  (req, res) => {
-    console.log(req.params);
-    const schedule_id = req.params.id;
-    
-    db.query('DELETE FROM schedules WHERE schedule_id=($1)', [schedule_id])
-    
-        .then(() => res.sendStatus(200))
-        .catch(err => {
-          console.log('Delete schedule error ', err);
-          res.sendStatus(500);
-        });
+router.delete("/:id", (req, res) => {
+  console.log(req.params);
+  const schedule_id = req.params.id;
 
+  db.query("DELETE FROM schedules WHERE schedule_id=($1)", [schedule_id])
+
+    .then(() => res.sendStatus(200))
+    .catch((err) => {
+      console.log("Delete schedule error ", err);
+      res.sendStatus(500);
+    });
 });
 
-// edit a schedule
+// edit a schedule title and description
+
 router.put("/:id", (req, res) => {
   console.log(req.body);
   const schedule_id = req.params.id;
-  const {title, description} = req.body;
-  const editSchedule  = [
-    title,
-    description
-  ];
+  const { title, description } = req.body;
+  const editSchedule = [title, description, schedule_id];
   console.log(editSchedule);
-  db.query("SELECT * FROM schedules WHERE schedule_id=($1)", [schedule_id])
+  db.query(
+    "UPDATE schedules SET title=($1), description=($2) WHERE schedule_id=($3)",
+    editSchedule
+  )
     .then(() => {
-      db.query("UPDATE schedules SET title=($1), description=($2)", editSchedule);
       res.sendStatus(200);
     })
     .catch((err) => {

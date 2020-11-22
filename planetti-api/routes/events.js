a35c16dd89acfbe87e065c26e06dd4b589fa289d
Alex-Vel <t9veal00@students.oamk.fi>
2020-11-22 22:49:43 +0200

    eddited put request for events

--- planetti-api/routes/events.js
+++ planetti-api/routes/events.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const router = express.Router({ mergeParams: true });
+//const router = express.Router({ mergeParams: true });
+const router = express.Router();
 const db = require("../db/index");
 
 // get events for schedule by schedule id
 
 router.get("/:id", (req, res) => {
   const schedule_id = req.params.id;
-console.log("selecting by id..");
+  console.log("selecting by id..");
   db.query("SELECT * FROM events WHERE schedule_id=($1)", [schedule_id]).then(
     (events) => {
       res.send(events.rows);
@@ -16,20 +17,20 @@ console.log("selecting by id..");
 
 // get events for schedule by schedule uuid
 
-router.get("/", (req, res) => {
-  const schedule_uuid = req.params.uuid;
-console.log("selecting by uuid. " + schedule_uuid);
-  db.query("SELECT schedule_id FROM schedules WHERE uuid=($1)", [
-    schedule_uuid,])
-    .then((schedule) => {
-    const schedule_id = (schedule.rows[0].schedule_id);
-    db.query("SELECT * FROM events WHERE schedule_id=($1)", [schedule_id]).then(
-      (events) => {
-        res.send(events.rows);
-      }
-    );
-  });
-});
+// router.get("/", (req, res) => {
+//   const schedule_uuid = req.params.uuid;
+// console.log("selecting by uuid. " + schedule_uuid);
+//   db.query("SELECT schedule_id FROM schedules WHERE uuid=($1)", [
+//     schedule_uuid,])
+//     .then((schedule) => {
+//     const schedule_id = (schedule.rows[0].schedule_id);
+//     db.query("SELECT * FROM events WHERE schedule_id=($1)", [schedule_id]).then(
+//       (events) => {
+//         res.send(events.rows);
+//       }
+//     );
+//   });
+// });
 
 // create an event for a schedule
 
@@ -68,9 +69,11 @@ router.put("/:id", (req, res) => {
   const event_id = req.params.id;
   const editEvent = req.body;
   console.log(editEvent);
-  db.query("SELECT event FROM events WHERE event_id=($1)", [event_id])
+  db.query("UPDATE events SET event=($2) WHERE event_id=($1)", [
+    event_id,
+    editEvent,
+  ])
     .then(() => {
-      db.query("UPDATE events SET event=($1)", [editEvent]);
       res.sendStatus(200);
     })
     .catch((err) => {

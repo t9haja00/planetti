8f8358b86bfff2bb334900d20999557a253d9f90
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-23 02:13:16 +0200

    Create useEFfect to fetch schedule data from API and Add Styles

--- planetti-ui/src/components/ViewSchedule.jsx
+++ planetti-ui/src/components/ViewSchedule.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
-import { ScheduleComponent, Inject, Week, ViewsDirective, ViewDirective } 
-   from '@syncfusion/ej2-react-schedule';
+import React, { useState, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
+import { ScheduleComponent, Inject, Week, ViewsDirective, ViewDirective }
+  from '@syncfusion/ej2-react-schedule';
+import { getScheduleByUUID } from '../services/scheduleService';
+import { DelayedRender } from './common/DelayedRender';
 import "@syncfusion/ej2-base/styles/material.css";
 import "@syncfusion/ej2-buttons/styles/material.css";
 import "@syncfusion/ej2-calendars/styles/material.css";
@@ -14,36 +17,66 @@ import "@syncfusion/ej2-react-schedule/styles/material.css";
 
 const ViewSchedule = ({ match }) => {
 
+  const [schedule, setSchedule] = useState(null);
   const [events, setEvents] = useState([
-  {
-    Id: 1,
-    Subject: 'Meeting - 1',
-    StartTime: new Date(2020, 10, 19, 10, 0),
-    EndTime: new Date(2020, 10, 19, 12, 30),
-    IsAllDay: false,
-  },
-]);
+    {
+      Id: 1,
+      Subject: 'Meeting - 1',
+      StartTime: new Date(2020, 10, 19, 10, 0),
+      EndTime: new Date(2020, 10, 19, 12, 30),
+      IsAllDay: false,
+    },
+  ]);
+
+  useEffect(() => {
+    const getSchedule = async () => {
+      const { data } =
+        await getScheduleByUUID('9efa2ebd-46a2-4312-9982-13ac12ead6ed');
+      setSchedule(data[0]);
+    }
+
+    getSchedule();
+  }, []);
 
 
-const onActionBegin = args => {
-   console.log(args);
-}
+  const onActionBegin = args => {
+    console.log(args);
+  }
 
-return (
-  <div className='container'>
-    <ScheduleComponent
-      height='750px'
-      showWeekNumber={true}
-      firstDayOfWeek={1}
-      eventSettings={{ dataSource: events}}
-      actionBegin={onActionBegin}>
-      <ViewsDirective>
-         <ViewDirective option='Week'></ViewDirective>
-      </ViewsDirective>
-      <Inject services={[Week]} />
-    </ScheduleComponent>
-  </div>
-);
+
+  return (
+    <>
+      {schedule ?
+        <div className='m-5'>
+          <div className='row my-4'>
+            <div className='col-2'>
+              <Card>
+                <Card.Header>
+                  {schedule.title}
+                </Card.Header>
+                <Card.Body>
+                  {schedule.description}
+                </Card.Body>
+              </Card>
+            </div>
+            <div className='col border rounded p-1 shadow' >
+              <ScheduleComponent
+                height='650px'
+                showWeekNumber={true}
+                firstDayOfWeek={1}
+                eventSettings={{ dataSource: events }}
+                actionBegin={onActionBegin}>
+                <ViewsDirective>
+                  <ViewDirective option='Week'></ViewDirective>
+                </ViewsDirective>
+                <Inject services={[Week]} />
+              </ScheduleComponent>
+            </div>
+          </div>
+        </div>
+        : <DelayedRender />}
+    </>
+  );
 }
 
 export default ViewSchedule;

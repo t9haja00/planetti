4ff1807e074032e21aeab4244c14034800e863ff
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-23 03:49:15 +0200

    Import getEventsByScheduleId and map the eventsData into syncfusion schedule readable format

--- planetti-ui/src/components/ViewSchedule.jsx
+++ planetti-ui/src/components/ViewSchedule.jsx
@@ -3,6 +3,7 @@ import { Card } from 'react-bootstrap';
 import { ScheduleComponent, Inject, Week, ViewsDirective, ViewDirective }
   from '@syncfusion/ej2-react-schedule';
 import { getScheduleByUUID } from '../services/scheduleService';
+import { getEventsByScheduleId } from './../services/eventService';
 import { DelayedRender } from './common/DelayedRender';
 import "@syncfusion/ej2-base/styles/material.css";
 import "@syncfusion/ej2-buttons/styles/material.css";
@@ -18,31 +19,32 @@ import "@syncfusion/ej2-react-schedule/styles/material.css";
 const ViewSchedule = ({ match }) => {
 
   const [schedule, setSchedule] = useState(null);
-  const [events, setEvents] = useState([
-    {
-      Id: 1,
-      Subject: 'Meeting - 1',
-      StartTime: new Date(2020, 10, 19, 10, 0),
-      EndTime: new Date(2020, 10, 19, 12, 30),
-      IsAllDay: false,
-    },
-  ]);
+  const [events, setEvents] = useState(null);
 
   useEffect(() => {
     const getSchedule = async () => {
-      const { data } =
+      const { data: scheduleData } =
         await getScheduleByUUID('9efa2ebd-46a2-4312-9982-13ac12ead6ed');
-      setSchedule(data[0]);
+
+      const { data: eventsData } = await getEventsByScheduleId(scheduleData[0].schedule_id);
+      const eventsArr = eventsData.map(e => {
+        e.event.StartTime = new Date(e.event.StartTime);
+        e.event.EndTime = new Date(e.event.EndTime);
+        e.event.IsAllDay = (e.event.IsAllDay == 'true');
+      
+        return e.event;
+      });
+
+      setSchedule(scheduleData[0]);
+      setEvents(eventsArr);
     }
 
     getSchedule();
   }, []);
 
-
   const onActionBegin = args => {
     console.log(args);
   }
-
 
   return (
     <>

68606fc89163c1eaa3c508e62630b0c2813c8084
Alex-Vel <t9veal00@students.oamk.fi>
2020-11-24 11:47:43 +0200

    edited divider bar, plus format

--- planetti-ui/src/components/SingleSchedule.jsx
+++ planetti-ui/src/components/SingleSchedule.jsx
@@ -1,56 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card , Button } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import styles from "../assets/css/userpage.module.css";
- function SingleSchedule(props, deleteSchedule) {
 
-    return(
-
-
-        <Card style={{width:'10rm'}}> 
-        <Card.Body
-        className={styles.card_style}
-        >
+function SingleSchedule(props, deleteSchedule) {
+  return (
+    <Card style={{ width: "10rm" }}>
+      <Card.Body className={styles.card_style}>
         <div key={props.schedule_id}>
-        <Link
-        to={`/view-schedule/${props.uuid}`}
-        style={{ color: "inherit", textDecoration: "inherit" }}
-      >
-        <Card.Title>
-        <h3 className={styles.truncated}>{props.title}</h3>
-        </Card.Title>
-        <p className={styles.truncated}>{props.description}</p>
-        </Link>
+          <Link
+            to={`/view-schedule/${props.uuid}`}
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <Card.Title>
+              <h3 className={styles.truncated}>{props.title}</h3>
+            </Card.Title>
+            <p className={styles.truncated}>{props.description}</p>
+          </Link>
         </div>
-      
+        <hr></hr>
         <div className={styles.buttonBar}>
-        <button
-        className={"btn btn-danger"}
-        onClick={() => {
-          props.deleteSchedule(props.schedule_id);
-        }}
-      >
-        Delete
-      </button>
-      {/* edit button */}
-      <button
-      className={"btn"}
-      style={{backgroundColor:"#05386B",color:"white"}}
-        onClick={() => {
-          props.editSchedule([
-            props.schedule_id,
-            props.title,
-            props.description,
-          ]);
-        }}
-      >
-        Edit
-      </button>
-      </div>
-    
-        </Card.Body>
-        </Card>
-    )
-
-    }
+          <button
+            className={"btn btn-danger"}
+            onClick={() => {
+              props.deleteSchedule(props.schedule_id);
+            }}
+          >
+            Delete
+          </button>
+          {/* edit button */}
+          <button
+            className={"btn"}
+            style={{ backgroundColor: "#05386B", color: "white" }}
+            onClick={() => {
+              props.editSchedule([
+                props.schedule_id,
+                props.title,
+                props.description,
+              ]);
+            }}
+          >
+            Edit
+          </button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
 export default SingleSchedule;

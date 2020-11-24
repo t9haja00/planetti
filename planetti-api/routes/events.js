03fdc3ecbce5d891d2c4c8d8167d4144b98dd271
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-24 19:18:31 +0200

    Create event API to work with UrlAdaptor

--- planetti-api/routes/events.js
+++ planetti-api/routes/events.js
@@ -1,84 +1,103 @@
 const express = require("express");
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 const db = require("../db/index");
 
-// get events for schedule by schedule id
+router.post('/', (req, res) => {
 
-router.get("/:id", (req, res) => {
-  const schedule_id = req.params.id;
-  console.log("selecting by id..");
-  db.query("SELECT * FROM events WHERE schedule_id=($1)", [schedule_id]).then(
-    (events) => {
-      res.send(events.rows);
-    }
-  );
-});
+  const { schedule_id } = req.body.params;
+  const { action, added, changed, deleted } = req.body;
 
-// get events for schedule by schedule uuid
+  db.query("SELECT * FROM events WHERE schedule_id=($1)", [schedule_id])
+    .then(({ rows }) => {
+      const eventsArr = rows.map(e => e.event);
 
-router.get("/", (req, res) => {
-  const schedule_uuid = req.params.uuid;
-console.log("selecting by uuid. " + schedule_uuid);
-  db.query("SELECT schedule_id FROM schedules WHERE uuid=($1)", [
-    schedule_uuid,])
-    .then((schedule) => {
-    const schedule_id = (schedule.rows[0].schedule_id);
-    db.query("SELECT * FROM events WHERE schedule_id=($1)", [schedule_id]).then(
-      (events) => {
-        res.send(events.rows);
+      if (!action) return res.send(eventsArr);
+
+      if (action === 'insert' ||
+        (action == 'batch' && added.length > 0)) {
+
+        const value = (action === 'insert') ?
+          value : added[0];
+
+        const startTime = new Date(value.StartTime);
+        const endTime = new Date(value.EndTime);
+
+        const maxId = rows.length > 0 ?
+          rows.reduce((acc, row) =>
+            Math.max(acc, row.event.Id), 0) : 0;
+
+        const newEvent = {
+          Id: maxId + 1,
+          StartTime: startTime,
+          EndTime: endTime,
+          Subject: value.Subject,
+          Location: value.Location,
+          Description: value.Description,
+          IsAllDay: value.IsAllDay,
+          StartTimeZone: value.StartTimezone,
+          EndTimeZone: value.EndTimezone,
+          RecurrenceRule: value.RecurrenceRule,
+          RecurrenceID: value.RecurrenceID,
+          RecurrenceException: value.RecurrenceException
+        }
+
+        db.query("INSERT INTO events (event, schedule_id) VALUES ($1, $2)", [newEvent, schedule_id])
+          .then( _ => res.send(newEvent))
+          .catch(err => res.sendStatus(500));
       }
-    );
-  });
-});
 
-// create an event for a schedule
+      if (action === 'update' ||
+        (action === 'batch' && changed.length > 0)) {
 
-router.post("/", (req, res) => {
-  const eventObject = req.body.event;
-  const schedule_id = req.body.schedule_id;
+        const value = (action === 'update') ?
+          value : changed[0];
 
-  const newEvent = [eventObject, schedule_id];
-  console.log(newEvent);
+        const filterData = rows.filter(r => r.event.Id === value.Id);
 
-  db.query("INSERT INTO events (event, schedule_id) VALUES ($1, $2)", newEvent)
-    .then(() => res.sendStatus(201))
-    .catch((err) => {
-      console.log("Create new event error ", err);
-      res.sendStatus(500);
-    });
-});
+        const { event_id, event } = filterData[0];
 
-//delete an event
+        if (filterData.length > 0) {
+          const startTime = new Date(value.StartTime);
+          const endTime = new Date(value.EndTime);
 
-router.delete("/:id", (req, res) => {
-  console.log(req.params);
-  const event_id = req.params.id;
+          event.StartTime = startTime,
+            event.EndTime = endTime,
+            event.Subject = value.Subject,
+            event.Location = value.Location,
+            event.Description = value.Description,
+            event.IsAllDay = value.IsAllDay,
+            event.StartTimeZone = value.StartTimezone,
+            event.EndTimeZone = value.EndTimezone,
+            event.RecurrenceRule = value.RecurrenceRule,
+            event.RecurrenceID = value.RecurrenceID,
+            event.RecurrenceException = value.RecurrenceException
+        }
 
-  db.query("DELETE FROM events WHERE event_id=($1)", [event_id])
+        db.query("UPDATE events SET event=($1) WHERE event_id=($2)", [
+          event,
+          event_id,
+        ])
+          .then( _ => res.send(event))
+          .catch(err => res.sendStatus(500));
+      }
 
-    .then(() => res.sendStatus(200))
-    .catch((err) => {
-      console.log("Delete event error ", err);
-      res.sendStatus(500);
-    });
-});
+      if (action == "remove" || (action == "batch" && deleted.length > 0)) {
 
-// edit an event object
-router.put("/:id", (req, res) => {
-  const event_id = req.params.id;
-  const editEvent = req.body.event;
-  console.log(editEvent);
-  db.query("UPDATE events SET event=($2) WHERE event_id=($1)", [
-    event_id,
-    editEvent,
-  ])
-    .then(() => {
-      res.sendStatus(200);
+        for (let event of deleted) {
+          const filterData = rows.find(r => r.event.Id === event.Id);
+          const { event_id } = filterData;
+
+          db.query("DELETE FROM events WHERE event_id=($1)", [event_id])
+            .then( _ => res.send(filterData))
+            .catch((err) => {
+              res.sendStatus(500);
+            });
+        }
+      }
+
     })
-    .catch((err) => {
-      console.log("edit event error ", err);
-      res.sendStatus(500);
-    });
+    .catch(e => res.sendStatus(500));
 });
+
 
 module.exports = router;

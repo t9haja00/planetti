145ad546fea2ec11c77093583be57a5dbd11618a
Karim Abdelrahman <ka_mo_hassan@hotmail.com>
2020-11-25 11:23:41 +0200

    Adjust the navbar to work with all user state scenarios

--- planetti-ui/src/components/common/NavBar.jsx
+++ planetti-ui/src/components/common/NavBar.jsx
@@ -21,84 +21,91 @@ const NavBar = ({
     setShowMenu(false);
   }
 
-  let containerStyle = "";
+  let containerStyle = 'container position-absolute py-2';
   const notLoggedIn = (() => {
-    if (!userName && location.pathname !== '/login') {
-      containerStyle = "container position-absolute py-2";
+    const { pathname } = location;
+
+    // Home page/view schedule for non loggedin users
+    if (!userName && pathname.toLowerCase() !== '/login') {
+
+      if (pathname.includes('schedule')) containerStyle = '';
+
       return (
-        <>
-          <Link
-            className={`${styles.login} ${styles['navbar-link']} btn mx-1`}
-            to='/login'
-            name='login'
-          >Sign in</Link>
-          <Link
-            className={`${styles.join} ${styles['navbar-link']} btn rounded-pill mx-1`}
-            to='/join'
-            name='join'
-          >Sing up</Link>
-        </>
+        <div className={containerStyle} style={{ right: 0, left: 0 }}>
+          <Navbar variant="light" className={`${styles.navbar} justify-content-between`}>
+            <Navbar.Brand>
+              <Link
+                className={styles['navbar-brand']}
+                to='/'>{siteName}</Link>
+            </Navbar.Brand>
+            <Nav>
+              <Link
+                className={`${styles.login} ${styles['navbar-link']} btn mx-1`}
+                to='/login'
+                name='login'
+              >Sign in</Link>
+              <Link
+                className={`${styles.join} ${styles['navbar-link']} btn rounded-pill mx-1`}
+                to='/'
+                name='join'
+              >Sing up</Link>
+            </Nav>
+          </Navbar>
+        </div>
       );
     }
+
+    // login page
+    return null;
+
   })();
 
   const loggedIn = (() => {
     if (userName) {
       return (
-        <NavDropdown
-          title={userName}
-          className={`${styles['nav-dropdown']}`}
-          alignRight
-          show={showMenu}
-          onMouseEnter={handleShowMenu}
-          onMouseLeave={handleHideMenu}
-        >
-          <NavDropdown.Item as={Link} to="/">
-            Main page
-          </NavDropdown.Item>
-          <NavDropdown.Item 
-            as={Link} 
-            to="/user-settings/user-profile">
-            User settings
-          </NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item >
-            <button
-              onClick={onLogout}
-            >Sign out</button>
-          </NavDropdown.Item>
-        </NavDropdown>
+        <div style={{ right: 0, left: 0 }}>
+          <Navbar variant="light" className={`${styles.navbar} justify-content-between`}>
+            <Navbar.Brand>
+              <Link
+                className={styles['navbar-brand']}
+                to='/'>{siteName}</Link>
+            </Navbar.Brand>
+            <Nav>
+              <NavDropdown
+                title={userName}
+                className={`${styles['nav-dropdown']}`}
+                alignRight
+                show={showMenu}
+                onMouseEnter={handleShowMenu}
+                onMouseLeave={handleHideMenu}
+              >
+                <NavDropdown.Item as={Link} to="/">
+                  Main page
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  as={Link}
+                  to="/user-settings/user-profile">
+                  User settings
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item >
+                  <button
+                    onClick={onLogout}
+                  >Sign out</button>
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar>
+        </div>
       );
     }
   })();
 
   return (
-    <div className={containerStyle} style={{ right: 0, left: 0 }}>
-      {location.pathname === '/login' &&
-        <div className="text-center pt-4 pb-3">
-          <Link
-            to="/"
-            className="text-decoration-none">
-            <p
-              className={styles['navbar-brand']}
-            >Planetti</p>
-          </Link>
-        </div>
-      }
-      {location.pathname !== '/login' &&
-        <Navbar variant="light" className={`${styles.navbar} justify-content-between`}>
-          <Navbar.Brand>
-            <Link
-              className={styles['navbar-brand']}
-              to='/'>{siteName}</Link>
-          </Navbar.Brand>
-          <Nav>
-            {notLoggedIn}
-            {loggedIn}
-          </Nav>
-        </Navbar>
-      }
-    </div>
+    <>
+      { notLoggedIn}
+      { loggedIn}
+    </>
   );
 }
 

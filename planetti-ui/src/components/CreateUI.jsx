ae5984775ad4294185dacc6952843e8a50318b85
Jami Hannus <t9haja00@students.oamk.fi>
2020-12-04 19:30:50 +0200

    Updated CreateUi file

--- planetti-ui/src/components/CreateUI.jsx
+++ planetti-ui/src/components/CreateUI.jsx
@@ -1,61 +1,60 @@
-import Select from "react-select";
-
-const options = [
-  { value: "text", label: "Text" },
-  { value: "number", label: "Only Numbers" },
-  { value: "email", label: "Email" },
-  { value: "url", label: "Url" },
-];
-const customStylesSelect = {
-  menu: (provided, state) => ({
-    ...provided,
-    width: state.selectProps.width,
-    borderBottom: "250px dotted pink",
-    color: state.selectProps.menuColor,
-    padding: 20,
-  }),
-  control: (_, { selectProps: { width } }) => ({
-    width: width,
-  }),
-  singleValue: (provided, state) => {
-    const opacity = state.isDisabled ? 0.5 : 1;
-    const transition = "opacity 300ms";
-    return { ...provided, opacity, transition };
-  },
-};
-
+import {
+  DropdownButton,
+  Dropdown,
+  Button,
+  FormControl,
+  InputGroup,
+} from "react-bootstrap";
+import styles from "../assets/css/delete-account.module.css";
 function CreateUI(props) {
-  return props.map((el, i) => (
+  return props.customFields.map((el, i) => (
     <div key={i}>
-      <div>
-        <input
-          name={el.value + " "}
-          type="text"
-          value={el.value}
-          onChange={(e) => this.handleChangesInput(e, i)}
-        />
-        <Select
-          value={el.selectedOption}
-          onChange={(e) => this.handleChangeSelect(e, i)}
-          options={options}
-          width="200px"
-          menuColor="red"
-          styles={customStylesSelect}
-          name="select"
-        />
-        Is Mandatory? :
-        <input
-          name="isMandatory"
-          type="checkbox"
-          defaultChecked={false}
-          checked={el.isMandatory}
-          onChange={(e) => this.handleCheckBox(e, i)}
-        />
-        <input
-          type="button"
-          value="remove"
-          onClick={this.removeClick.bind(this, i)}
-        />
+      <div className="form-group">
+        <div className={styles.wrapRow}>
+          <InputGroup>
+            <FormControl
+              placeholder="Your fields name in schedule"
+              aria-label="Your fields name in schedule"
+              aria-describedby="basic-addon2"
+              name={el.value + " "}
+              onChange={(e) => this.handleChangesInput(e, i)}
+            />
+            <DropdownButton
+              focusFirstItemOnShow={true}
+              as={InputGroup.Append}
+              variant="outline-secondary"
+              title={props.customFields[i].type}
+              id="input-group-dropdown-2"
+              onSelect={(e) => this.handleChangeSelect(e, i)}
+            >
+              <Dropdown.Item eventKey="number">Numbers</Dropdown.Item>
+              <Dropdown.Item eventKey="email">Email</Dropdown.Item>
+              <Dropdown.Item eventKey="text">Text</Dropdown.Item>
+              <Dropdown.Item eventKey="url">URL</Dropdown.Item>
+            </DropdownButton>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Mandatory?</InputGroup.Text>
+              <InputGroup.Checkbox
+                aria-label="Checkbox for following text input"
+                name="mandatory"
+                type="checkbox"
+                defaultChecked={false}
+                checked={el.mandatory}
+                onChange={(e) => this.handleCheckBox(e, i)}
+              />
+            </InputGroup.Prepend>
+            <InputGroup.Append>
+              <Button
+                variant="outline-secondary"
+                type="button"
+                value="remove"
+                onClick={this.removeClick.bind(this, i)}
+              >
+                Delete
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </div>
       </div>
     </div>
   ));
